4dfb0e06b6a2a77a4c6aa098dfe1f9506d366e48
Raphael Ottoni <rapha.ottoni@gmail.com>
2017-06-22 19:31:33 -0300

    adding 27 more stumps for positeve/negative prediction

--- boost/Boost.py
+++ boost/Boost.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy
 import math
 
 class Boost:
@@ -7,8 +6,9 @@ class Boost:
     # Class constructor
     def __init__(self,nsamples,nstumps):
         print("Creating boosting algorithm with " + str(nsamples)+ " samples and " + str(nstumps) + " stubs" )
-        self.weights = np.full((nsamples,1),1/nsamples)
+        self.weights = np.full((nsamples,1),1.0/nsamples)
         self.nstumps = nstumps
+        self.nsamples = nsamples
         self.chosen_stumps = []
         self.chosen_alphas = []
         self.chosen_errors = []
@@ -21,58 +21,104 @@ class Boost:
         return 1/2 * math.log((1-error)/(error))
 
     # Run the boosting algorithm for the class
-    def execute(self,samples):
+    def build_classifier(self,samples):
 
         # runs n times, where n is the number of desirable stumps
         for i in range(self.nstumps):
             self.choose_stump(samples)
             self.update_weights(self.chosen_stumps[-1],samples)
+            print(self.weights[-4:])
 
-        print(self.chosen_stumps)
-        print(self.chosen_errors)
-        print(self.chosen_alphas)
+        print("Stumps: "+ str(self.chosen_stumps))
+        print("Errors: "+ str(self.chosen_errors))
+        print("Alphas: " +str(self.chosen_alphas))
 
+
+    # Classify an sample
+    def classify(self,sample):
+        class_sample = 0.0
+
+        #iterate over the number of stumps chosen
+        for i in range(len(self.chosen_stumps)):
+
+            predict_stump_class = 0.0
+            # get stump attributes
+            characteristic,value,prediction = self.stumps[self.chosen_stumps[i]]
+
+            # find the stump classification for this sample
+            if (sample[characteristic] == value ):
+              predict_stump_class = 1 if prediction == "positive" else 0
+            else:
+              predict_stump_class = 0 if prediction == "positive" else 1
+
+            #print(predict_stump_class)
+
+            # Add weak classifier contribution
+            #print(str(self.chosen_alphas[i]) + "*" + str(predict_stump_class))
+            #predict_stump_class = 1 if prediction == "positive" else  -1
+
+            #class_sample += self.chosen_alphas[i]*predict_stump_class
+            class_sample += self.chosen_alphas[i]*predict_stump_class
+
+        #print("ta aki")
+        #print(class_sample)
+        resultado =  1 if class_sample > 0.0 else -1
+        return resultado
 
     # Generator for Tic Tac Toe possible stumps
     def buildStumps(self):
 
         stumps = []
-        # adding two inicial stumps for all positive and all negative
-        #stumps.append([0,'true'])
 
         # for each position in Tic Tac Toe matrix
         for i in range(9):
-            #For each label possibility
-            for j in ['o','x','b']:
-                stumps.append([i,j])
+
+            # Create  stumps both for positive predictions and negative predictions
+            for k in ['positive','negative']:
+                #For each label possibility
+                for j in ['o','x','b']:
+                    stumps.append([i,j,k])
         return stumps
 
     # Finds the next best stump candidate
-    # Returns the stump and the error associated
     def choose_stump(self,samples):
         stump_choosen_id = -1
         min_error = None
 
         # iterate over all possible stumps and chose the local best
-        for characteristic_id, (characteristic,value) in enumerate(self.stumps):
+        for characteristic_id, (characteristic,value, prediction) in enumerate(self.stumps):
 
-            error = 0.0
-            # iterate over all samples
-            for i,sample in enumerate(samples):
-                if (sample[characteristic] == value and sample[9] == "negative" ):
-                  error += self.weights[i][0]
-                elif (sample[characteristic] != value and sample[9] == "positive" ):
-                  error += self.weights[i][0]
+            if characteristic_id not in self.chosen_stumps:
+                error = 0.0
+                # iterate over all samples
+                #for i,sample in enumerate(samples):
+                #    #print(sample[characteristic],value,sample[9])
+                #    if (sample[characteristic] == value and sample[9] == "negative" ):
+                #      error += self.weights[i][0]
+                #    elif (sample[characteristic] != value and sample[9] == "positive" ):
+                #      error += self.weights[i][0]
 
-            # Keep the lowest error found
-            if min_error is None:
-                min_error = error
-                stump_choosen_id = characteristic_id
-            elif min_error > error:
-                min_error = error
-                stump_choosen_id = characteristic_id
+                # iterate over all samples
+                for i,sample in enumerate(samples):
+                    #print(sample[characteristic],value,sample[9])
+                    if (sample[characteristic] == value and sample[9] != prediction ):
+                      error += self.weights[i][0]
+                    elif (sample[characteristic] != value and sample[9] == prediction ):
+                      error += self.weights[i][0]
 
-        print("[Chosen] characteristic " + str(stump_choosen_id) + " errou : " + str(min_error) )
+                print("[test] characteristic(" + str(characteristic_id) + ")["+str(self.stumps[characteristic_id][0]) + ","  + str(self.stumps[characteristic_id][1]) +" ]   error: " + str(error) )
+                # Keep the lowest error found
+                    ##print("Comparando: " + str(min_error) + " ? " + str(error)+ ": " + str(min_error >= error)   )
+                if min_error is None:
+                    min_error = error
+                    stump_choosen_id = characteristic_id
+                elif min_error > error:
+                    #print("troquei")
+                    min_error = error
+                    stump_choosen_id = characteristic_id
+
+        print("[Chosen] characteristic(" + str(stump_choosen_id) + ")["+str(self.stumps[stump_choosen_id][0]) + ","  + str(self.stumps[stump_choosen_id][1]) + "," + str(self.stumps[stump_choosen_id][2])  +"]   error: " + str(min_error) )
+
 
         # Add the selected stumps statistics to the chosen arrays
         self.chosen_stumps.append(stump_choosen_id)
@@ -84,21 +130,19 @@ class Boost:
     # Update weight function regarding the chosen stump
     def update_weights(self,stump_id,samples):
 
-        #old_weights =  copy.copy(self.weights)
-
         # Get the stump attributes
-        characteristic,value = self.stumps[stump_id]
+        characteristic,value,prediction = self.stumps[stump_id]
         error = self.chosen_errors[-1]
 
 
         # Iterate over each sample to adjust its weight
         for i,sample in enumerate(samples):
-          if (sample[characteristic] == value and sample[9] == "negative" ):
-            self.weights[i][0] = self.weights[i][0]/2 * 1/error
-          elif (sample[characteristic] != value and sample[9] == "positive" ):
-            self.weights[i][0] = self.weights[i][0]/2 * 1/error
+          if (sample[characteristic] == value and sample[9] != prediction ):
+            self.weights[i][0] = (self.weights[i][0]/2) * (1/error)
+          elif (sample[characteristic] != value and sample[9] == prediction ):
+            self.weights[i][0] = (self.weights[i][0]/2) * (1/error)
           else:
-            self.weights[i][0] = self.weights[i][0]/2 * 1/(1 - error)
+            self.weights[i][0] = (self.weights[i][0]/2) * (1/(1 - error))
           #print("new weight: " + str(self.weights[i][0]))
 
 
--- main.py
+++ main.py
@@ -4,10 +4,40 @@ import datareader
 
 games = datareader.readGames('./dataset/tic-tac-toe.data')
 
+boost = Boost(len(games),7)
+boost_1 = Boost(len(games),1)
 boost_2 = Boost(len(games),2)
 boost_4 = Boost(len(games),4)
+boost_5 = Boost(len(games),4)
 boost_8 = Boost(len(games),8)
 boost_16 = Boost(len(games),16)
+boost_64 = Boost(len(games),64)
 
-#boost_2.execute(games)
-boost_16.execute(games)
+#print(boost.weights)
+boost.build_classifier(games)
+#boost_5.build_classifier(games[:10])
+acertos = 0
+erros =  0
+#
+for sample in games:
+    #print(sample[9])
+    #print("oi: " + str(boost.classify(sample)))
+    if sample[9] == "positive" and boost.classify(sample) == 1:
+        acertos += 1
+        #print("acertei")
+    elif sample[9] == "negative" and boost.classify(sample) == -1:
+        acertos += 1
+        #print("acertei")
+    else:
+        erros += 1
+        #print("errei")
+    #print("Classifiquei: " + str(boost.classify(sample)))
+
+print("Certos: " + str(acertos) +", errors: " +str(erros) )
+#result = 0.0
+#for i in boost.weights:
+#    result += i[0]
+#
+#print(boost.weights)
+#print("Peso: " + str(result))
+
